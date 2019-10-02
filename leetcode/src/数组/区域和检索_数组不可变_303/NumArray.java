b7d69b903d0cb02a3e038cafe45b2aadf8449e71
lhg <rabbitlhg@163.com>
2019-10-02 21:56:45 +0800

    add 303

--- leetcode/src/数组/区域和检索_数组不可变_303/NumArray.java
+++ leetcode/src/数组/区域和检索_数组不可变_303/NumArray.java
@@ -2,10 +2,11 @@ package 数组.区域和检索_数组不可变_303;
 
 public class NumArray {
 
-    private int[] sum; // sum[i]存储前i个元素和，sum[0] = 0
+    private int[] sum; // sum[i]存储前i个元素和，sum[0] = 0，sum[i]存储nums[0...i-1]的和
 
     public NumArray(int[] nums) {
         sum = new int[nums.length + 1];
+        sum[0] = 0;
         for (int i = 0; i < nums.length; i++) {
             sum[i + 1] = sum[i] + nums[i];
         }
